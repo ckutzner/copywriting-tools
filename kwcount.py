77793e8638046cdab2e04e6498024f0c60f9bd5f
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-17 11:22:57 +0100

    some unfinished ideas

--- kwcount.py
+++ kwcount.py
@@ -1,5 +1,6 @@
 """ module for keyword count """
 import spacy
+from spacy.matcher import Matcher
 
 def kw_count(infile, kw_file):
     """ expects input: infile = a markdown file, kw_file = a text file containing one keyword per line
@@ -9,7 +10,20 @@ def kw_count(infile, kw_file):
     keyfile = open(kw_file)
 
     text = textfile.read()
-    keys = keyfile.readlines()
+    keys_raw = keyfile.readlines()
+
+    #strip newline char from raw keys
+    keys = []
+    for line in keys_raw:
+        keys.append(line.rstrip())
+
+    # set up nlp - or am I supposed to put this in a different function?
+    nlp = spacy.load("de_core_news_sm")
+    doc = nlp(text)
+
+    # build patterns for matching
+
+    # collect match count
 
     print("Keywords found: {}".format(key_found))
     return key_found
