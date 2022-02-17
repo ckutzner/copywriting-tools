21baecd273303cd126f1a06e84d8955e70291be3
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-17 11:56:04 +0100

    some work on building the matching pattern

--- kwcount.py
+++ kwcount.py
@@ -6,11 +6,8 @@ def kw_count(infile, kw_file):
     """ expects input: infile = a markdown file, kw_file = a text file containing one keyword per line
     :returns: how many times do the keywords contained in kw_file occur in the infile?
     """
-    textfile = open(infile)
-    keyfile = open(kw_file)
-
-    text = textfile.read()
-    keys_raw = keyfile.readlines()
+    text = open(infile).read()
+    keys_raw = open(kw_file).readlines()
 
     #strip newline char from raw keys
     keys = []
@@ -19,9 +16,14 @@ def kw_count(infile, kw_file):
 
     # set up nlp - or am I supposed to put this in a different function?
     nlp = spacy.load("de_core_news_sm")
+    # prepare text for processing
     doc = nlp(text)
 
     # build patterns for matching
+    kwpat = []
+    for k in keys:
+        kw = nlp(k)
+        kwpat.append([w.lemma_ for w in kw])
 
     # collect match count
 
