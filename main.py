62554ff7f1e3c1713f842bc80d8691659b1769e4
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-28 17:34:39 +0100

    reduced the number of arguments to call main.py
    script now expects to find files with reqs, forbidden and moneykw in the
    same directory as text to be analyzed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """ call with filename of file to be analyzed """
 import sys
+import os
 import textstat
 import stats
 import structure
@@ -8,9 +9,9 @@ import listcheck
 
 #read infile 
 infile = sys.argv[1]
-reqfile = sys.argv[2]
-forbidden = sys.argv[3]
-desired = sys.argv[4]
+reqfile = str(os.path.dirname(infile) + "/reqs.txt")
+forbidden = str(os.path.dirname(infile) + "/forbidden.txt")
+desired = str(os.path.dirname(infile) + "/moneykw.txt")
 
 stats.wordcount(infile)
 stats.readability(infile)
