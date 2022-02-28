b6c09aa3799d87d4926ace4b4fa9a25ab2f8e6c5
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-28 17:26:07 +0100

    added crude and convoluted call to incorporate files with desired and forbidden words

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import sys
 import textstat
 import stats
 import structure
+import listcheck
 
 #read infile 
 infile = sys.argv[1]
 reqfile = sys.argv[2]
+forbidden = sys.argv[3]
+desired = sys.argv[4]
 
 stats.wordcount(infile)
 stats.readability(infile)
 structure.wellformed(infile, reqfile)
+print(listcheck.listcheck(infile, forbidden))
+print(listcheck.listcheck(infile, desired))
