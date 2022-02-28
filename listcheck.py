2361e2f6e457d5d328f0dbbd73119e7ac61fe41b
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-28 15:22:19 +0100

    file objects: listfile, infile

--- listcheck.py
+++ listcheck.py
@@ -2,11 +2,25 @@
 import spacy
 
 def listcheck(infile, listfile):
-    """TODO: Docstring for listcheck.
-    :returns: TODO
+    """ reads listfile, looks for matches of the lemma in infile, counts matches
+    arguments: infile - an text or markdown file, listfile - a textfile with one word to check per line
+    :returns: each word from the list that was found + word as a list of tuples
 
     """
-    pass
+    # read listfile
+    lfile = listfile.read()
+    # read infile & prepare for processing by spacy - externalize this to a different function for speed reasons!
+    ifile = infile.read()
+
+    # make pattern for matching
+    lines = lfile.split("\n")
+
+    # match infile
+    # close files, return matches and count
+    ifile.close()
+    lfile.close()
+    return matchcount
 
 if __name__ == "__main__":
-    print("test for listcheck not written yet!")
+    for l in listcheck("testdata/testtext2.md", "testdata/forbidden.txt"):
+        print("Forbidden word {} found {} times".format(l[0], l[1]));
