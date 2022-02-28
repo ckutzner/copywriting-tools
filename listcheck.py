0a8a862b49277168129bd338ece1d58a16047dd2
Camilla Kutzner <develop@camilla-kutzner.de>
2022-02-28 14:53:56 +0100

    list of desired words, rudimentary listcheck function
    list of desirable words ("money keywords")
    beginning work on function for checking for a list of words
    
    beginning work on test

---
+++ listcheck.py
@@ -0,0 +1,12 @@
+""" module to check for forbidden and desired words """
+import spacy
+
+def listcheck(infile, listfile):
+    """TODO: Docstring for listcheck.
+    :returns: TODO
+
+    """
+    pass
+
+if __name__ == "__main__":
+    print("test for listcheck not written yet!")
