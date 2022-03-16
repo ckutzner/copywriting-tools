1b35d2f5c715c21be3ca199d2bf9b2359c1a7df0
Camilla Kutzner <develop@camilla-kutzner.de>
2022-03-16 11:20:19 +0100

    code cleanup

--- stats.py
+++ stats.py
@@ -2,22 +2,30 @@
 import textstat
 
 def wordcount(infile):
-    """ expects as input: a file; count total words in the text, return count as an integer"""
-    file = open(infile)
-    text = file.read()
+    """ count words
+
+    :infile: a text file 
+    :returns: wordcount as an integer
+    """
+    
+    with open(infile) as file:
+        text = file.read()
+
     wc = textstat.lexicon_count(text)
     
-    print("Text ist {} Worte lang.".format(wc))
-    file.close()
     return wc
 
 def readability(infile):
-    """ expects as input: a file; returns Flesch index and Wiener sachtextformel index as floats"""
-    file = open(infile)
-    text = file.read()
+    """ readability stats 
+
+    :infile: a text file; 
+    :returns: Flesch index and Wiener sachtextformel index as floats
+    """
+    
+    with open(infile) as file:
+        text = file.read()
+    
     flesch = textstat.flesch_reading_ease(text)
     stf = textstat.wiener_sachtextformel(text, 1)
 
-    print ("Flesch Index: {0:5.2f}\nWiener Sachtextformel: {1:5.2f}".format(flesch, stf))
-    file.close()
     return flesch, stf
