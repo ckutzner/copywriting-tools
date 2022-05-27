9a5c5e8cadd8b0681e4160c84a5734d6e45077c3
Camilla Kutzner <develop@camilla-kutzner.de>
2022-05-27 17:09:27 +0200

    added SpaCy document generation

--- docgen.py
+++ docgen.py
@@ -3,6 +3,7 @@ import spacy
 
 nlp = spacy.load("de_core_news_sm") # load SpaCy pipeline
 
+
 class MainDoc:
 
     """Returns the whole document as a SpaCy object to perform matching operations on. """
@@ -10,11 +11,24 @@ class MainDoc:
     def __init__(self, docfile):
         """:docfile: document to be processed, a text or markdown file. """
         
+        self.docfile = docfile
+
+
+    def docgen(self):
+        """Generate SpaCy doc.
+        :returns: spacy doc
+
+        """
         # prepare document
         with open(self.docfile, encoding = "utf-8") as txt:
             text = txt.read()
-
+        
         # prepare text for processing
         doc = nlp(text)
 
         return doc
+
+
+if __name__ == "__main__":
+    text = MainDoc("testdata/testtext.md")
+    print(text.docgen()[0:10])
